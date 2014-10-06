cf9cf878bcef02d35be5be3e201f0d3b9a225e72
Niranjan Kumar <niranjan93@berkeley.edu>
2014-10-06 11:24:38 -0700

    [RNAdam-32] Added EM initialization code. Addresses RNAdam Issue Number 32.

--- RNAdam-core/src/test/scala/org/bdgenomics/RNAdam/algorithms/quantification/QuantifyTestSuite.scala
+++ RNAdam-core/src/test/scala/org/bdgenomics/RNAdam/algorithms/quantification/QuantifyTestSuite.scala
@@ -17,6 +17,7 @@
  */
 package org.bdgenomics.RNAdam.algorithms.quantification
 
+import scala.math
 import org.apache.spark.rdd.RDD
 import org.bdgenomics.adam.util.SparkFunSuite
 
@@ -34,6 +35,43 @@ class QuantifyTestSuite extends SparkFunSuite {
     assert(classCounts.filter((x: (Long, Long)) => x._1 == 1).first() === (1, 80))
     assert(classCounts.filter((x: (Long, Long)) => x._1 == 2).first() === (2, 60))
     assert(classCounts.filter((x: (Long, Long)) => x._1 == 3).first() === (3, 75))
+  }
+
+  sparkTest("test of initializeEM") {
+    val equivalenceClassCounts: RDD[(Long, Long)] = sc.parallelize(
+      Seq((1, 45),
+        (2, 52),
+        (3, 49)))
+    val equivalenceClassToTranscript: RDD[(Long, Iterable[String])] = sc.parallelize(
+      Seq((2, Seq("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m")),
+        (3, Seq("a", "b", "c", "d", "e", "f", "g")),
+        (1, Seq("a", "b", "c", "d", "e"))))
+    val result: RDD[(Long, Iterable[(String, Double)])] = Quantify.initializeEM(equivalenceClassCounts, equivalenceClassToTranscript)
+    assert(result.count() === 3)
+    val ec1p: RDD[(Long, Iterable[(String, Double)])] = result.filter((x: (Long, Iterable[(String, Double)])) => x._1 == 1)
+    assert(ec1p.count() === 1)
+    val ec1: (Long, Iterable[(String, Double)]) = ec1p.first()
+    assert(ec1._1 === 1)
+    assert(ec1._2.size === 5)
+    assert(ec1._2.forall((x: (String, Double)) => {
+      math.abs(x._2 - 9.0) < 1e-3
+    }))
+    val ec2p: RDD[(Long, Iterable[(String, Double)])] = result.filter((x: (Long, Iterable[(String, Double)])) => x._1 == 2)
+    assert(ec2p.count() === 1)
+    val ec2: (Long, Iterable[(String, Double)]) = ec2p.first()
+    assert(ec2._1 === 2)
+    assert(ec2._2.size === 13)
+    assert(ec2._2.forall((x: (String, Double)) => {
+      math.abs(x._2 - 4.0) < 1e-3
+    }))
+    val ec3p: RDD[(Long, Iterable[(String, Double)])] = result.filter((x: (Long, Iterable[(String, Double)])) => x._1 == 3)
+    assert(ec3p.count() === 1)
+    val ec3: (Long, Iterable[(String, Double)]) = ec3p.first()
+    assert(ec3._1 === 3)
+    assert(ec3._2.size === 7)
+    assert(ec3._2.forall((x: (String, Double)) => {
+      math.abs(x._2 - 7.0) < 1e-3
+    }))
   }
 
 }
